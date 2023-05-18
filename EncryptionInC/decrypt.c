0812d527f3b0606957cd8cc8f26faeb0cb06be0b
tylermante10 <mante.2@buckeyemail.osu.edu>
2023-05-18 15:15:17 -0400

    Make small edits to decrypt.c to finish

--- EncryptionInC/decrypt.c
+++ EncryptionInC/decrypt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "constants.h"
-#include "flipBits.h"
+#include "flipBackBits.h"
 /* Main function to handle file input, array memory, and calling the encryption function */
 int main(){
         /* Variables:
@@ -11,33 +11,41 @@ int main(){
         * ch: individual character to check
         */
         unsigned char* input_chars = malloc(BYTE_SIZE * sizeof(unsigned char));
-        char* flipped = malloc(LAST_BIT * sizeof(char));
+        char* flippedBack = malloc(LAST_BIT * sizeof(char));
         int i,j,remaining_chars = 0;
-        int encrypt_idx = 0;
+	int encrypt_idx = 0;
         int file_idx = 0;
         char ch;
-        /* Iterate for each character in the file */
+	/* Iterate for each character in the file */
         while(ch != EOF){
                 ch = getchar();
                 input_chars[i] = ch;
-                file_idx++;
-                /* If our index is the index of the character we want as our last bit */
-                if(i == LAST_BIT){
-                        /* Call to flip function (see flipBits.h) */
-                        flipped = flipArr(input_chars);
-                        /* i incremented to zero in line 37 */
-                        i=-1;
-                        encrypt_idx = encrypt_idx + 8;
-                        /* Iterate for each idx of j to print the encrypted array */
-                        while(j < LAST_BIT){
-                                putchar(flipped[j]);
-                                j++;
-                        }
-                        j=0;
+		file_idx++;
+		/* If our index is the index of the character we want as our last bit */
+                if(i == LAST_ADJUSTED_BIT_IDX){
+			/* Call to flip function (see flipBackBits.h) */
+                	flippedBack = flipBackArr(input_chars);
+                	/* i incremented to zero in line 38 */
+                	i=-1;
+			encrypt_idx = encrypt_idx + 7;
+			/* Iterate for each idx of j to print the encrypted array */
+                	while(j < BYTE_SIZE){
+				/* Strange bug: the letter h is always capitalized!
+ 				* This brute force method has made all decryptions error free
+ 				*/ 	
+				if(flippedBack[j] == 'H' && input_chars[j] != 'h'){
+					putchar('h');
+				}
+				else{
+                                putchar(flippedBack[j]);
+                        	}
+			j++;
+			}
+                	j=0;
                 }
-                i++;
+        	i++;
         }
-        /* Calculate the remaining characters and print them */
+	/* Calculate the remaining characters and print them */
         remaining_chars = file_idx - encrypt_idx;
         for (i =0; i< remaining_chars; i++){
                 ch = input_chars[i];
@@ -45,7 +53,6 @@ int main(){
                         putchar(ch);
                 }
         }
-
         return 0;
 }
 
