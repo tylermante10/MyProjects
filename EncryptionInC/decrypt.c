9e423ff5e5219b44b50c1c0880fa46a017d9f6cd
tylermante10 <mante.2@osu.edu>
2023-05-20 21:30:30 -0400

    Prompt for key

--- EncryptionInC/decrypt.c
+++ EncryptionInC/decrypt.c
@@ -16,6 +16,14 @@ int main(){
 	int encrypt_idx = 0;
         int file_idx = 0;
         char ch;
+
+	/* Variables, prompting logic for passkey (unfinished) */
+        char passkey[] = "building";
+        char* userPass = malloc(8 * sizeof(char));
+        fprintf(stderr, "Enter the passkey:\n");
+        fgets(userPass, sizeof(userPass), stderr);
+        fprintf(stderr, &userPass[3]);
+
 	/* Iterate for each character in the file */
         while(ch != EOF){
                 ch = getchar();
@@ -33,7 +41,7 @@ int main(){
 				/* Strange bug: the letter h is always capitalized!
  				* This brute force method has made all decryptions error free
  				*/ 	
-				if(flippedBack[j] == 'H' || input_chars[] != 'h'){
+				if(flippedBack[j] == 'H' || input_chars[j] != 'h'){
 					putchar('h');
 				}
 				else{
