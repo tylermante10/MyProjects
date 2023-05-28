d922d9eef6e6f5839bab4b46f0d23e28d71c3b42
tylermante10 <mante.2@buckeyemail.osu.edu>
2023-05-28 12:14:40 -0400

    Reads last char of array

--- EncryptionX86/encryption.s
+++ EncryptionX86/encryption.s
@@ -10,7 +10,8 @@
 .format:
 	.string "%8s"
 .final:
-	.string "%s"
+	.string "%c"
+
 # .next:
 #	.byte 'o' 
 .data
@@ -29,19 +30,25 @@ main:
         pushq  %r12      #r12 is going to be index
         pushq  %r13      #r13 will point to beginning of array
 
-	movq %rbp, %r13
+	movq %rbp, %r13	 #r13 set up to arr[0]
 
 	movq $.format, %rdi
 	movq $x, %rsi
         xorq %rax , %rax
         call scanf
-
-	movq $x, %rsi
+	
+	movq $7, %r12
+	movq $x, %r13
+	movq (%r13, %r12, 1), %rsi
 	movq $.final, %rdi
 	xorq %rax, %rax
-	call printf
+	call printf	
+	 
+	#movq $x, %rsi
+	#movq $.final, %rdi
+	#xorq %rax, %rax
+	#call printf
 
-	xorq %rax, %rax
         popq %r13
         popq %r12
         leave
